d1a68e3c319449e8002338919ffdeaa40fd5cc59
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-07-10 14:56:54 +0000

    feat: support usage as codecompanion extension

---
+++ lua/codecompanion/_extensions/spinner/init.lua
@@ -0,0 +1,12 @@
+local codecompanion_spinner = require("codecompanion-spinner")
+
+---@class CodeCompanion.Extension
+---@field setup fun(opts: table) Function called when extension is loaded
+---@field exports? table Functions exposed via codecompanion.extensions.your_extension
+local M = {}
+
+function M.setup(opts)
+	codecompanion_spinner.setup(opts)
+end
+
+return M
